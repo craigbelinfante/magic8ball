3f940f9ca1d12719310118211b3060b05a75cf91
craig <craigbasketball@aol.com>
2021-06-27 12:47:16 -0700

    first commit

---
+++ 8ball/8ballViewController.swift
@@ -0,0 +1,25 @@
+//
+//  8ballViewController.swift
+//  8ball
+//
+//  Created by Craig Belinfante on 6/27/21.
+//
+
+import UIKit
+
+class _ballViewController: UIViewController {
+    
+    @IBOutlet weak var magicBall: UIImageView!
+    @IBOutlet weak var answersLabel: UILabel!
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
+
